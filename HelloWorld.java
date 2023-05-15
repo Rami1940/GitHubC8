220cfca61785edaa1a2d2c3b89c79c0c291c2faf
Mando <114784167+Rami1940@users.noreply.github.com>
2023-05-15 16:42:02 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,16 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Wolrd!");
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("Hello world in Intellij");
-        }
-// this needs to be a while loop
-            int num = 0;
-            while(num<10) {
-                System.out.println("The number is "+num);
-                num++;
-            }
-                    System.out.println("The is the end of work in Github");
+        /*
+        some
+        codes
+        for
+        loginPage
+        */  
         }
     }
 
