a5ad56740ebace7707e83315703fdf2ef9da7366
Rami1940 <ahadramijr@gmail.com>
2023-05-15 16:21:50 -0400

    Conflict

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Wolrd!");
         for (int i = 0; i <10 ; i++) {
-            System.out.println(i);
+            System.out.println("Hello world in Intellij");
         }
 // this needs to be a while loop
             int num = 0;
