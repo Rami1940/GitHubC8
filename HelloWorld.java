56cd362b213023cbadb84d57cfe902bb6db6bdf6
Mando <114784167+Rami1940@users.noreply.github.com>
2023-05-16 11:13:06 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         /*
-        Some
+        some
         codes
         for
-        LandingPage
+        loginPage
         */  
         }
     }
